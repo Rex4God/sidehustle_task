7e35aa91ec55917bac4d61321665f7bb24ec1ffc
Agamuyi Precious Edobor <66228350+Rex4God@users.noreply.github.com>
2021-06-25 14:05:56 +0100

    Add files via upload

---
+++ sidehustle_task/range.php
@@ -0,0 +1,24 @@
+<!doctypehtml>
+<html>
+<head>
+<title>RANGE IN PHP</title>
+</head>
+<body>
+
+<h1> this is range function</h1>
+
+<?php
+function myRange(){
+$number =range(1,50);
+echo "<pre>";
+print_r($number);
+echo "</pre>";
+}
+myRange();
+ 
+ 
+?>
+
+
+</body>
+<html>
---
+++ sidehustle_task/sum.php
@@ -0,0 +1,19 @@
+<!doctypehtml>
+<html>
+<head>
+<title> sum of numbers</title>
+</head>
+<body>
+<h1><center> This is sum of arrays of numbers in PHP</center></h1>
+<?php
+function sum(){
+$arr =[1,3,5,6,7,8,9,10];
+print array_sum($arr);
+}
+echo sum();
+?>
+
+
+
+</body>
+</html>
